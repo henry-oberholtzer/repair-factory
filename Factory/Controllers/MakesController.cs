63762629ac69434b0a2a8f1f78271c3aa9420839
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-02-13 21:55:28 -0800

    makes are fully functional, but lack implementation with mechanics

--- Factory/Controllers/MakesController.cs
+++ Factory/Controllers/MakesController.cs
@@ -11,7 +11,28 @@ public class MakesController : Controller
   {
     _db = db;
   }
+  public Dictionary <string, object> MakeFormModel(Make make, string action)
+  {
+    return new Dictionary<string, object> {
+      {"Make", make},
+      {"Action", action}
+    };
+  }
 
+  public bool CheckForDuplicates(Make make)
+  {
+#nullable enable
+  Make? existing = _db.Makes.FirstOrDefault(m => m.Name.ToLower() == make.Name.ToLower());
+#nullable disable
+  if (existing == null)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+  }
   public async Task<IActionResult> Index()
   {
     List<Make> makes = await _db.Makes
@@ -21,11 +42,61 @@ public class MakesController : Controller
     return View(makes);
   }
 
-  [HttpPost]
   public ActionResult Create()
   {
     
-    return View();
+    return View(MakeFormModel(new Make(), "Create"));
+  }
+
+  [HttpPost]
+  public ActionResult Create(Make make)
+  {
+    if(!ModelState.IsValid)
+    {
+      return View(MakeFormModel(make, "Create"));
+    }
+    if (CheckForDuplicates(make) == false)
+    {
+      _db.Makes.Add(make);
+      _db.SaveChanges();
+    }
+    return RedirectToAction("Index");
+  }
+
+  public ActionResult Edit(int id)
+  {
+    Make target = _db.Makes.FirstOrDefault(m => m.MakeId == id);
+    return View(MakeFormModel(target, "Edit"));
+  }
+
+  [HttpPost]
+  public ActionResult Edit(Make make)
+  {
+    if(!ModelState.IsValid)
+    {
+      return View(MakeFormModel(make, "Edit"));
+    }
+    if (CheckForDuplicates(make) == false)
+    {
+      _db.Makes.Update(make);
+      _db.SaveChanges();
+    }
+    return RedirectToAction("Index");
+  }
+
+  [HttpPost]
+  public ActionResult Delete(int id)
+  {
+    Make target = _db.Makes
+    .Include(m => m.MakeMechanics)
+    .Include(m => m.MakeVehicles)
+    .FirstOrDefault(m => m.MakeId == id);
+    if(target.MakeVehicles.Count == 0 && target.MakeMechanics.Count == 0)
+    {
+      _db.Makes.Remove(target);
+      _db.SaveChanges();
+    }
+    return RedirectToAction("Index");
   }
 
 }
