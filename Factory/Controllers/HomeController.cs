df7bc4bd33256cfbe5c51d542223bb301fe275ee
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-02-13 21:59:57 -0800

    added to main page

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -19,9 +19,11 @@ namespace Factory.Controllers;
     {
       List<Vehicle> vehicles = await _db.Vehicles.Include(v => v.VehicleMechanics).ToListAsync();
       List<Mechanic> mechanics = await _db.Mechanics.Include(v => v.VehicleMechanics).ToListAsync();
+      List<Make> makes = await _db.Makes.Include(m => m.MakeMechanics).Include(m => m.MakeVehicles).ToListAsync();
       Dictionary<string, object> model = new() {
         {"vehicles", vehicles},
-        {"mechanics", mechanics}
+        {"mechanics", mechanics},
+        {"makes", makes}
       };
       return View(model);
     }
